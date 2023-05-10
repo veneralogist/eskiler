51c37ad2e0b1094842e9648483afedd964cce965
veneralogist <bilimsel314@gmail.com>
2023-05-10 19:36:35 +0300

    hakkımda ve home sayfası güncellendi

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var _ = require("lodash");
 const mongoose = require("mongoose");
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const homeStartingContent =
+  "Bizler Ziya Uslu'nun ailesi ve sevenleri olarak, Saygıdeğer Merhum Büyüğümüz Ziya Uslu'nun anısını yaşatmak ve özlem gidermek için burada anılarından oluşan bir demeç hazırlamak istedik. Güzel anılar hoş sohbetlerde canlansın sitemizde de tarihe kazınsın diye bu girişimde bulunduk. Bizler yeni nesil olarak sadece Ziya Uslu'nun değil onun ışığında kıymetli tüm aile büyüklerimizi anmak, gelecek nesil üyelerimize anlatmak bizlere de anımsatmak için bir anılar defteri oluşturduk. Burada toplandığımız sıcak aile gecelerinde çay eşliğinde yapılan derin muhabbetlerde anılan hoş günleri yâd edeceğiz.Sevgiler";
+const aboutContent =
+  "Biz Ziya Uslu'nun Ailesiyiz ve onu ve onunla göçüp giden tatlı hatıraları yeniden yaşatmak , ve her daim canlı tutmak için el ele verdik bu amaçla bir anılar seçkisi oluşturduk, bu sayfada geçen her bir isim kaderin titrek ağlarında bize ve gönlümüze değmeyi başarmış kutlu insanlardır.Sen her kimsen, torun, kuzen, amca, dayı, yada bambaşka biri bil ki bu site Unutmayanların sitesidir.";
 
 const app = express();
 
